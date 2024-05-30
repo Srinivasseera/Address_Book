797b32abb906addecea3e274386739d98ba8f3ef
Srinivasseera <srinivasseera7@gmail.com>
2024-05-30 11:01:12 +0530

    Initializing Address Book

---
+++ Address_Book/Address_Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book
+{
+    public class Address_Main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+        }
+    }
+}
